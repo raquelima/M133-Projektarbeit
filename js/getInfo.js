39545070a2d3b0bca446c4860be28898be1174c6
raquelima <rrlima.raquel@gmail.com>
2022-04-03 14:57:26 +0200

    Implemented http requests for berufe and klassen; All berufe are visible in the dropdown; When selecting beruf, the klassen for this beruf are visible but doesn't always work; When selecting a beruf, the beruf_id is saved in the localstorage; When selection a klasse the table is visible but only shows one day

---
+++ js/getInfo.js
@@ -0,0 +1,56 @@
+$.getJSON( "http://sandbox.gibm.ch/berufe.php", function( berufe ) {
+
+    berufe.forEach(beruf => {
+        $('#drop-berufsgrupe').append(`<option value='${beruf.beruf_id}'>${beruf.beruf_name}</option>`)
+    });
+
+    if (localStorage.getItem('beruf_id') != null){
+        $("#drop-berufsgrupe").val(localStorage.getItem('beruf_id'));
+    }
+
+});
+
+$( "#drop-berufsgrupe" ).change(function() {
+
+    localStorage.setItem('beruf_id', $('#drop-berufsgrupe').val());
+    
+    $.getJSON( `http://sandbox.gibm.ch/klassen.php?beruf_id=${$('#drop-berufsgrupe').val()}&format=JSON`, function( klassen ) {
+
+        klassen.forEach(klasse => {
+            $('#drop-klassenauswahl').append(`<option value='${klasse.klasse_id}'>${klasse.klasse_name}</option>`)
+        });
+});
+  });
+
+  $( "#drop-klassenauswahl" ).change(function() {
+
+    localStorage.setItem('klasse_id', $('#drop-klassenauswahl').val());
+    
+    $.getJSON( `http://sandbox.gibm.ch/tafel.php?klasse_id=${$('#drop-klassenauswahl').val()}&format=JSON`, function( data ) {
+
+        data.forEach(element => {
+            $('#table').html(`<table class="table">
+                <tr>
+                    <th>Datum</th>
+                    <th>Wochentag</th>
+                    <th>Von</th>
+                    <th>Bis</th>
+                    <th>Lehrer</th>
+                    <th>Fach</th>
+                    <th>Raum</th>
+
+                </tr>
+                <tr>
+                    <td>${element.tafel_datum}</td>
+                    <td>${element.tafel_wochentag}</td>
+                    <td>${element.tafel_von}</td>
+                    <td>${element.tafel_bis}</td>
+                    <td>${element.tafel_lehrer}</td>
+                    <td>${element.tafel_fach}</td>
+                    <td>${element.tafel_raum}</td>
+                </tr>
+            </table>`)
+        });
+});
+  });
+
