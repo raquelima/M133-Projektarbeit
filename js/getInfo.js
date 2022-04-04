6117c423ce560ae692a7cdbb4daf5839c5ecd4a4
raquelima <rrlima.raquel@gmail.com>
2022-04-04 09:21:31 +0200

    Moved the table to the HTML file and set the display to none. The table is only displayed when a klasse is selected. Only the body of the table is updated when changing class. Made comments for js file Lines starting

--- js/getInfo.js
+++ js/getInfo.js
@@ -1,64 +1,45 @@
+
+//getting all berufe for dropdown
 $.getJSON("http://sandbox.gibm.ch/berufe.php", function (berufe) {
 
+    //appending each to the dropdown
     berufe.forEach(beruf => {
         $('#drop-berufsgrupe').append(`<option value='${beruf.beruf_id}'>${beruf.beruf_name}</option>`)
     });
 
+    //checking localstorage for beruf_id
     if (localStorage.getItem('beruf_id') != null) {
+
+        //if not empty then set dropdown value to the id saved in the localstorage
         $("#drop-berufsgrupe").val(localStorage.getItem('beruf_id'));
     }
 
 });
 
+//getting all klassen for dropdown
 $.getJSON("http://sandbox.gibm.ch/klassen.php", function (klassen) {
 
+    //appending each to the dropdown
     klassen.forEach(klasse => {
         $('#drop-klassenauswahl').append(`<option value='${klasse.klasse_id}'>${klasse.klasse_name}</option>`)
     });
 
+    //if klasse_id saved in localstorage then
     if (localStorage.getItem('klasse_id') != null) {
+
+        //set dropdown value to the id saved in the localstorage
         $("#drop-klassenauswahl").val(localStorage.getItem('klasse_id'));
-    }
 
-});
+        //show table
+        $('#table').css("display", "inline");
 
-$("#drop-berufsgrupe").change(function () {
+        //get stundenplan for selected class 
+        $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${localStorage.getItem('klasse_id')}&format=JSON`, function (data) {
 
-    localStorage.setItem('beruf_id', $('#drop-berufsgrupe').val());
-
-    $('#drop-klassenauswahl').empty();
-
-    $('#drop-klassenauswahl').append(`<option value='0'> - please select - </option>`)
-
-    $.getJSON(`http://sandbox.gibm.ch/klassen.php?beruf_id=${$('#drop-berufsgrupe').val()}&format=JSON`, function (klassen) {
-
-        klassen.forEach(klasse => {
-            $('#drop-klassenauswahl').append(`<option value='${klasse.klasse_id}'>${klasse.klasse_name}</option>`)
-        });
-    });
-
-
-});
-
-
-$("#drop-klassenauswahl").change(function () {
-
-    localStorage.setItem('klasse_id', $('#drop-klassenauswahl').val());
-
-    $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${$('#drop-klassenauswahl').val()}&format=JSON`, function (data) {
-
-        data.forEach(element => {
-            $('#table').html(`<table class="table">
-                <tr>
-                    <th>Datum</th>
-                    <th>Wochentag</th>
-                    <th>Von</th>
-                    <th>Bis</th>
-                    <th>Lehrer</th>
-                    <th>Fach</th>
-                    <th>Raum</th>
-
-                </tr>
+            //fill table with new data
+            data.forEach(element => {
+                $('#tbody').html(`
+                
                 <tr>
                     <td>${element.tafel_datum}</td>
                     <td>${element.tafel_wochentag}</td>
@@ -67,8 +48,61 @@ $("#drop-klassenauswahl").change(function () {
                     <td>${element.tafel_lehrer}</td>
                     <td>${element.tafel_fach}</td>
                     <td>${element.tafel_raum}</td>
-                </tr>
-            </table>`)
+                </tr>`)
+            });
+        });
+    }
+
+});
+
+//in case dropdown changes 
+$("#drop-berufsgrupe").change(function () {
+
+    //set beruf_id from selected option in localstorage
+    localStorage.setItem('beruf_id', $('#drop-berufsgrupe').val());
+
+    //empty dropdown with klassen
+    $('#drop-klassenauswahl').empty();
+
+    //add select option 
+    $('#drop-klassenauswahl').append(`<option value='0'> - please select - </option>`)
+
+    //getting all klassen with the beruf_id from the beruf selected 
+    $.getJSON(`http://sandbox.gibm.ch/klassen.php?beruf_id=${$('#drop-berufsgrupe').val()}&format=JSON`, function (klassen) {
+
+        //appending each filtered klasse to dropdown
+        klassen.forEach(klasse => {
+            $('#drop-klassenauswahl').append(`<option value='${klasse.klasse_id}'>${klasse.klasse_name}</option>`)
+        });
+    });
+
+
+});
+
+//in case dropdown changes
+$("#drop-klassenauswahl").change(function () {
+
+    //set klasse_id from selected option in localstorage
+    localStorage.setItem('klasse_id', $('#drop-klassenauswahl').val());
+
+    //show table 
+    $('#table').css("display", "inline");
+
+    //getting all timetable data with the klasse_id from the klasse selected 
+    $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${$('#drop-klassenauswahl').val()}&format=JSON`, function (data) {
+
+        //fill table with new data
+        data.forEach(element => {
+            $('#tbody').html(`
+                <tr>
+                    <td>${element.tafel_datum}</td>
+                    <td>${element.tafel_wochentag}</td>
+                    <td>${element.tafel_von}</td>
+                    <td>${element.tafel_bis}</td>
+                    <td>${element.tafel_lehrer}</td>
+                    <td>${element.tafel_fach}</td>
+                    <td>${element.tafel_raum}</td>
+                </tr>`)
         });
     });
 });
