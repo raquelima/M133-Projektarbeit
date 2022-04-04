fe48b2f4c4d86e0e41e20f3042374b61c238bb7f
raquelima <rrlima.raquel@gmail.com>
2022-04-04 14:23:52 +0200

    Added week picker; Started implementig functions since its a requirement; implemented functionality of week picker;

--- js/getInfo.js
+++ js/getInfo.js
@@ -1,46 +1,96 @@
+var week = moment().week();
 
-//getting all berufe for dropdown
-$.getJSON("http://sandbox.gibm.ch/berufe.php", function (berufe) {
+var year = moment().year();
 
-    //appending each to the dropdown
-    berufe.forEach(beruf => {
-        $('#drop-berufsgrupe').append(`<option value='${beruf.beruf_id}'>${beruf.beruf_name}</option>`)
-    });
-
-    //checking localstorage for beruf_id
-    if (localStorage.getItem('beruf_id') != null) {
-
-        //if not empty then set dropdown value to the id saved in the localstorage
-        $("#drop-berufsgrupe").val(localStorage.getItem('beruf_id'));
-    }
-
+$('#back').click(function () {
+    week = week - 1;
+    weekNumber = week + "-" + year;
+    $('#week-input').html(weekNumber);
+    getTimeTable($('#drop-klassenauswahl').val(), weekNumber);
 });
 
-//getting all klassen for dropdown
-$.getJSON("http://sandbox.gibm.ch/klassen.php", function (klassen) {
 
-    //appending each to the dropdown
-    klassen.forEach(klasse => {
-        $('#drop-klassenauswahl').append(`<option value='${klasse.klasse_id}'>${klasse.klasse_name}</option>`)
+$('#forward').click(function () {
+    week = week + 1;
+    weekNumber = week + "-" + year;
+    $('#week-input').html(weekNumber);
+    getTimeTable($('#drop-klassenauswahl').val(), weekNumber);
+});
+
+
+$(() => {
+    $('#week-input').html(getCurrentWeek());
+
+    //getting all berufe for dropdown
+    getBerufe();
+
+    getKlassen(localStorage.getItem('beruf_id'));
+  
+
+})
+
+function getCurrentWeek() {
+    return moment().week() + "-" + moment().year()
+}
+
+function getBerufe() {
+    $.getJSON("http://sandbox.gibm.ch/berufe.php", function (berufe) {
+
+        //appending each to the dropdown
+        berufe.forEach(beruf => {
+            $('#drop-berufsgrupe').append(`<option value='${beruf.beruf_id}'>${beruf.beruf_name}</option>`)
+        });
+
+        //checking localstorage for beruf_id
+        if (localStorage.getItem('beruf_id') != null) {
+
+            //if not empty then set dropdown value to the id saved in the localstorage
+            $("#drop-berufsgrupe").val(localStorage.getItem('beruf_id'));
+        }
+
     });
+}
 
-    //if klasse_id saved in localstorage then
-    if (localStorage.getItem('klasse_id') != null) {
+function getKlassen(beruf_id) {
+    //getting all klassen for dropdown
+    $.getJSON(`http://sandbox.gibm.ch/klassen.php${beruf_id != null ? '?beruf_id=' + beruf_id : ''}`, function (klassen) {
 
-        //set dropdown value to the id saved in the localstorage
-        $("#drop-klassenauswahl").val(localStorage.getItem('klasse_id'));
+        //empty dropdown with klassen
+        $('#drop-klassenauswahl').empty();
 
-        //show table
-        $('#table').css("display", "inline");
+        //add select option 
+        $('#drop-klassenauswahl').append(`<option value='0'> - please select - </option>`)
 
-        //get stundenplan for selected class 
-        $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${localStorage.getItem('klasse_id')}&format=JSON`, function (data) {
+        //appending each to the dropdown
+        klassen.forEach(klasse => {
+            $('#drop-klassenauswahl').append(`<option value='${klasse.klasse_id}'>${klasse.klasse_name}</option>`)
+        });
 
-            //fill table with new data
-            data.forEach(element => {
-                $('#tbody').html(`
-                
-                <tr>
+        //if klasse_id saved in localstorage then
+        if (localStorage.getItem('klasse_id') != null) {
+
+            //set dropdown value to the id saved in the localstorage
+            $("#drop-klassenauswahl").val(localStorage.getItem('klasse_id'));
+
+            //show table
+            $('#table').css("display", "inline");
+
+            //get stundenplan for selected class 
+            getTimeTable(localStorage.getItem('klasse_id'), getCurrentWeek());
+        }
+
+    });
+}
+
+function getTimeTable(klasse_id, weekNumber) {
+
+    $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${klasse_id}&woche=${weekNumber}&format=JSON`, function (data) {
+
+        //fill table with new data
+        data.forEach(element => {
+
+            $('#tbody').html(`
+                <tr id="${element.tafel_id}">
                     <td>${element.tafel_datum}</td>
                     <td>${element.tafel_wochentag}</td>
                     <td>${element.tafel_von}</td>
@@ -49,11 +99,11 @@ $.getJSON("http://sandbox.gibm.ch/klassen.php", function (klassen) {
                     <td>${element.tafel_fach}</td>
                     <td>${element.tafel_raum}</td>
                 </tr>`)
-            });
         });
-    }
+    });
+}
 
-});
+
 
 //in case dropdown changes 
 $("#drop-berufsgrupe").change(function () {
@@ -61,23 +111,10 @@ $("#drop-berufsgrupe").change(function () {
     //set beruf_id from selected option in localstorage
     localStorage.setItem('beruf_id', $('#drop-berufsgrupe').val());
 
-    //empty dropdown with klassen
-    $('#drop-klassenauswahl').empty();
-
-    //add select option 
-    $('#drop-klassenauswahl').append(`<option value='0'> - please select - </option>`)
-
-    //getting all klassen with the beruf_id from the beruf selected 
-    $.getJSON(`http://sandbox.gibm.ch/klassen.php?beruf_id=${$('#drop-berufsgrupe').val()}&format=JSON`, function (klassen) {
-
-        //appending each filtered klasse to dropdown
-        klassen.forEach(klasse => {
-            $('#drop-klassenauswahl').append(`<option value='${klasse.klasse_id}'>${klasse.klasse_name}</option>`)
-        });
-    });
-
+    getKlassen(localStorage.getItem('beruf_id'));
 
 });
+
 
 //in case dropdown changes
 $("#drop-klassenauswahl").change(function () {
@@ -89,21 +126,8 @@ $("#drop-klassenauswahl").change(function () {
     $('#table').css("display", "inline");
 
     //getting all timetable data with the klasse_id from the klasse selected 
-    $.getJSON(`http://sandbox.gibm.ch/tafel.php?klasse_id=${$('#drop-klassenauswahl').val()}&format=JSON`, function (data) {
-
-        //fill table with new data
-        data.forEach(element => {
-            $('#tbody').html(`
-                <tr>
-                    <td>${element.tafel_datum}</td>
-                    <td>${element.tafel_wochentag}</td>
-                    <td>${element.tafel_von}</td>
-                    <td>${element.tafel_bis}</td>
-                    <td>${element.tafel_lehrer}</td>
-                    <td>${element.tafel_fach}</td>
-                    <td>${element.tafel_raum}</td>
-                </tr>`)
-        });
-    });
+    getTimeTable($('#drop-klassenauswahl').val(), getCurrentWeek());
 });
+
+
 
